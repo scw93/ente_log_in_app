7d0c7bfd20c431afc152dd24dd7c6be859963385
Piotr Adamczyk <piotr.a1993@gmail.com>
2023-02-07 13:50:15 +0100

    employee service

--- src/main/webapp/app/entities/employee/employee.service.ts
+++ src/main/webapp/app/entities/employee/employee.service.ts
@@ -9,7 +9,19 @@ import { ApplicationConfigService } from 'app/core/config/application-config.ser
 export class EmployeeService {
   constructor(private http: HttpClient, private applicationConfigService: ApplicationConfigService) {}
 
-  getAllEmployees(): Observable<{}> {
-    return this.http.get(this.applicationConfigService.getEndpointFor('api/employees'));
+  getAllEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.applicationConfigService.getEndpointFor('api/employees'));
+  }
+
+  create(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(this.applicationConfigService.getEndpointFor('api/employees'), employee);
+  }
+
+  update(id: number, employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.applicationConfigService.getEndpointFor('api/employees')}/${id}`, employee);
+  }
+
+  delete(id: number): Observable<any> {
+    return this.http.delete(`${this.applicationConfigService.getEndpointFor('api/employees')}/${id}`);
   }
 }
